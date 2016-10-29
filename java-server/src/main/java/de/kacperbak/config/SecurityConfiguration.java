b04a9fed5f1d79438d6dddb35a969301c2acf8f7
Kacper Bak <github@kacperbak.de>
2016-10-29 18:56:57 +0200

    enabled bcrypt

--- java-server/src/main/java/de/kacperbak/config/SecurityConfiguration.java
+++ java-server/src/main/java/de/kacperbak/config/SecurityConfiguration.java
@@ -1,13 +1,18 @@
 package de.kacperbak.config;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 /**
  * Created by bakka on 16.05.16.
@@ -47,10 +52,23 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
     /**
      * Override to configure user-details service
      * @param auth
-     * @throws Exception
+     * @throws Exception - If no user has been found
      */
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-        auth.userDetailsService(userDetailsService);
+        auth.authenticationProvider(authProvider());
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder(){
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public DaoAuthenticationProvider authProvider() {
+        DaoAuthenticationProvider authProvider = new DaoAuthenticationProvider();
+        authProvider.setUserDetailsService(userDetailsService);
+        authProvider.setPasswordEncoder(passwordEncoder());
+        return authProvider;
     }
 }
--- java-server/src/main/java/de/kacperbak/repository/UserRepositoryImpl.java
+++ java-server/src/main/java/de/kacperbak/repository/UserRepositoryImpl.java
@@ -1,14 +1,21 @@
 package de.kacperbak.repository;
 
 import de.kacperbak.domain.UserDetailsImpl;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Repository;
 
+import javax.annotation.PostConstruct;
 import java.util.HashMap;
-import java.util.HashSet;
 import java.util.Map;
 import java.util.Set;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 /**
  * Created by bakka on 16.05.16.
@@ -16,13 +23,28 @@ import java.util.Set;
 @Repository
 public class UserRepositoryImpl implements UserRepository {
 
-    private Map<String,UserDetailsImpl> userMap;
+    @Autowired
+    private PasswordEncoder passwordEncoder;
 
-    public UserRepositoryImpl() {
-        this.userMap = new HashMap<>();
-        Set<GrantedAuthority> set = new HashSet<>();
-        set.add(new SimpleGrantedAuthority("ROLE_USER"));
-        userMap.put("kacper", new UserDetailsImpl("password", "kacper", set, true));
+    private static final Logger LOGGER = LoggerFactory.getLogger(UserRepositoryImpl.class);
+
+    private Map<String,UserDetailsImpl> userMap = new HashMap<>();
+
+    @PostConstruct
+    private void bootstrapUsers(){
+
+        // Authorities
+        Set<GrantedAuthority> authorities = Stream.of(new SimpleGrantedAuthority("ROLE_USER")).collect(Collectors.toSet());
+
+        // User password
+        String userName = "kacper";
+        String userPasswordClearText = "password";
+        String userPasswordBCryptEncoded = passwordEncoder.encode(userPasswordClearText);
+        LOGGER.debug("password in clear text: '{}'", userPasswordClearText);
+        LOGGER.debug("password encoded: '{}'", userPasswordBCryptEncoded);
+
+        // Add 'User' to repository
+        userMap.put(userName, new UserDetailsImpl(userPasswordBCryptEncoded, userName, authorities, true));
     }
 
     @Override
